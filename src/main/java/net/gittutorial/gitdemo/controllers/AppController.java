0d94ea32f1e9af462a0362cbb5fae8083e24f6d1
SOUMIKBOSE560 <soumikbose560@gmail.com>
2024-05-12 23:07:45 +0530

    2ndl commit

--- src/main/java/net/gittutorial/gitdemo/controllers/AppController.java
+++ src/main/java/net/gittutorial/gitdemo/controllers/AppController.java
@@ -8,7 +8,12 @@ public class AppController {
 
     @GetMapping("/test1")
     public String test1(){
-        return "This is a testing message;";
+        return "This is a testing message";
+    }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "2nd commit in main branch";
     }
 
 }
